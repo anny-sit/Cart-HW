f74468112cba5472624905cbbfbac53a7db501f3
anny-sit <annysit@yandex.ru>
2024-11-16 09:42:00 +0300

    initial commit

---
+++ src/main/java/pro/sky/skypro_spring_cart/SkyproSpringCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_spring_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCartApplication.class, args);
+	}
+
+}
